0c80907a7b71b2b332642ebb57565d632fe391bf
Velani <rafaelvelaniofc@gmail.com>
2025-02-28 13:38:59 -0300

    Update 12_funcoes.py

--- 12_funcoes.py
+++ 12_funcoes.py
@@ -20,4 +20,38 @@ def par_impar(numero):
 print(par_impar(3))
 print(par_impar(10))
 print(par_impar(35))
-print(par_impar(102934))
+print(par_impar(102934), '\n--------')
+
+#-----------------
+
+usuario = input('Qual seu nome: ')
+
+def criar_saudacao(saudacao):
+    def saudar(nome):
+        return f'{saudacao}, {nome}'
+    return saudar
+
+bom_dia = criar_saudacao('Bom dia')
+boa_noite = criar_saudacao("Boa noite")
+boa_tarde = criar_saudacao("Boa tarde")
+
+print(bom_dia(usuario))
+print(boa_noite(usuario))
+print(boa_tarde(usuario), '\n--------')
+
+#-----------------
+num = input('Qual o numero a ser multiplicado: ')
+num = int(num)
+
+def multiplicator(multiplicador):
+    def multiplicar(numero):
+        return numero * multiplicador
+    return multiplicar
+
+duplicar = multiplicator(2)
+triplicar = multiplicator(3)
+quadruplicar = multiplicator(4)
+print(duplicar(num))
+print(triplicar(num))
+print(quadruplicar(num))
+
