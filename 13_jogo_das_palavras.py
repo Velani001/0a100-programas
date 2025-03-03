7a1041c99b1d3f83657c42a2d512de631e5f8a38
Velani <rafaelvelaniofc@gmail.com>
2025-03-03 12:24:24 -0300

    Create 13_jogo_das_palavras.py

---
+++ 13_jogo_das_palavras.py
@@ -0,0 +1,19 @@
+perguntas = [
+    {
+    'Pergunta': 'Quanto é 2+2',
+    'Opções': ['1', '2', '3', '4', '5'],
+    'Resposta': '4',
+    },
+        {
+    'Pergunta': 'Quanto é 5x5',
+    'Opções': ['25', '20', '22', '15'],
+    'Resposta': '25',
+    },
+        {
+    'Pergunta': 'Quanto é 10/2',
+    'Opções': ['1', '2', '3', '4', '5'],
+    'Resposta': '5',
+    },
+]
+for pergunta in perguntas:
+    ...
